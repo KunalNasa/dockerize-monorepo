e2acf15d4395ca3b398d2752e9d211299fbb4b2d
KunalNasa <kunalnasa.dev@gmail.com>
2025-03-24 22:44:34 +0530

    removed bug for cicd testing

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,6 @@ import { client } from "@repo/db/config";
 
 export default async function Home() {
   const users = await client.user.findMany();
-  fdfdf
   return (
     <div>
       {JSON.stringify(users)}
