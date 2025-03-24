90da1713735429a55f8b71281022d5d01cbc1163
KunalNasa <kunalnasa.dev@gmail.com>
2025-03-24 22:41:59 +0530

    introduced bug for testing code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,9 @@ import { client } from "@repo/db/config";
 
 export default async function Home() {
   const users = await client.user.findMany();
+  fdfdf
   return (
     <div>
-      cvkcvjk
       {JSON.stringify(users)}
     </div>
   );
